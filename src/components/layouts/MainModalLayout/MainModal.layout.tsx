21ea2ced647290e3103ec536b3f0ed629432be50
Jose Erney Ospina <jose@avenfor.com>
2022-10-16 15:49:58 -0500

    ( Chore )[ SDK ]: Check SDK

--- src/components/layouts/MainModalLayout/MainModal.layout.tsx
+++ src/components/layouts/MainModalLayout/MainModal.layout.tsx
@@ -9,13 +9,38 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../store/store";
 import {setToggle} from "../../../store/toggle.slice";
 import {UserBalanceMolecule} from "../../molecules/UserBalanceMolecule/UserBalance.molecule";
+import {useContractWrite, useWaitForTransaction} from "@web3modal/react";
+import {soulAbi} from "../../../../configs/smartContract.config";
+import {useNavigate} from "react-router-dom";
 
+
+const addressNFT = "0x2cFe378354FBC7E15f4E69AD6F077f619dBb865f";
+const owner = "0xf61585d10622b799Fd05f8a9baB50211B563cEa6"
 
 export const MainModalLayout = () => {
 
     // const showModal = true;
     const showModal: boolean = useSelector((state: RootState) => state.toggle.value)
     const dispatch = useDispatch()
+    const navigate = useNavigate();
+
+    const { data, error, write } = useContractWrite({
+        addressOrName: '0x1a0E25D5B68cd8233AC7c787E82D269Ae618C798',
+        contractInterface: soulAbi,
+        functionName: 'addAmount',
+        args: [addressNFT], //times 10 cuz decimals, gotta fix this
+    });
+
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    });
+
+    const sendTx = async() => {
+        console.log('yo')
+        await write().then(() => {
+            navigate('/player')
+        });
+    }
 
     return (
         <>
@@ -41,6 +66,7 @@ export const MainModalLayout = () => {
                                 backgroundColor={theme.TOKENS.S3.i900}
                                 borderColor={theme.TOKENS.S3.i900}
                                 color='white'
+                                onClick={sendTx}
                             />
                         </MainModalButtonsWrapper>
                     </MainModalContentWrapper>
